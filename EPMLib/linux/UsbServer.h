71bd159a07ad5efe3030a453cedc0c0476bede06
Roy, Biswajit <biswroy@qti.qualcomm.com>
2026-02-26 02:06:07 +0530

    further update license headers
    Signed-off-by: Roy, Biswajit <biswroy@qti.qualcomm.com>

--- EPMLib/linux/UsbServer.h
+++ EPMLib/linux/UsbServer.h
@@ -1,38 +1,7 @@
 #pragma once
 
-// Confidential and Proprietary – Qualcomm Technologies, Inc.
-
-// NO PUBLIC DISCLOSURE PERMITTED:  Please report postings of this software on public servers or websites
-// to: DocCtrlAgent@qualcomm.com.
-
-// RESTRICTED USE AND DISCLOSURE:
-// This software contains confidential and proprietary information and is not to be used, copied, reproduced, modified
-// or distributed, in whole or in part, nor its contents revealed in any manner, without the express written permission
-// of Qualcomm Technologies, Inc.
-
-// Qualcomm is a trademark of QUALCOMM Incorporated, registered in the United States and other countries. All
-// QUALCOMM Incorporated trademarks are used with permission.
-
-// This software may be subject to U.S. and international export, re-export, or transfer laws.  Diversion contrary to U.S.
-// and international law is strictly prohibited.
-
-// Qualcomm Technologies, Inc.
-// 5775 Morehouse Drive
-// San Diego, CA 92121 U.S.A.
-// Copyright © 2013-2016 Qualcomm Technologies, Inc.
-// All rights reserved.
-// Qualcomm Technologies Confidential and Proprietary
-
-/*
-   $Id: //depot/DM/dev/qxdm-qt/QUsb/UsbServer.h#17 $
-   $Header: //depot/DM/dev/qxdm-qt/QUsb/UsbServer.h#17 $
-   $Date: 2017/08/31 $
-   $DateTime: 2017/08/31 12:26:33 $
-   $Change: 1000981 $
-   $File: //depot/DM/dev/qxdm-qt/QUsb/UsbServer.h $
-   $Revision: #17 $
-   %Author: msimpson %
-   */
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause
 
 #include "qtusb_global.h"
 
