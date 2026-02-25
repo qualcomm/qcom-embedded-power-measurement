fc628e97a4e690030d5fd94ec08c4e67de432d98
Roy, Biswajit <biswroy@qti.qualcomm.com>
2026-02-26 01:25:17 +0530

    update license headers
    Signed-off-by: Roy, Biswajit <biswroy@qti.qualcomm.com>

--- PowerChart/move.bat
+++
@@ -1,3 +0,0 @@
-move ..\__Builds\x64\Debug\bin\PowerChart.lib ..\__Builds\x64\Debug\lib\PowerChart.lib
-move ..\__Builds\x64\Release\bin\PowerChart.lib ..\__Builds\x64\Release\lib\PowerChart.lib
-exit /b 0
