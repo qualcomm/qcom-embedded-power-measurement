9141f71f5bf127bbfdb67277570a2c0d6391fa2e
Roy, Biswajit <biswroy@qti.qualcomm.com>
2026-02-27 02:18:07 +0530

    further update license headers
    Signed-off-by: Roy, Biswajit <biswroy@qti.qualcomm.com>

--- QCommonConsole/LogInterface.cpp
+++ QCommonConsole/LogInterface.cpp
@@ -1,37 +1,5 @@
-
-// Confidential and Proprietary – Qualcomm Technologies, Inc.
-
-// NO PUBLIC DISCLOSURE PERMITTED:  Please report postings of this software on public servers or websites
-// to: DocCtrlAgent@qualcomm.com.
-
-// RESTRICTED USE AND DISCLOSURE:
-// This software contains confidential and proprietary information and is not to be used, copied, reproduced, modified
-// or distributed, in whole or in part, nor its contents revealed in any manner, without the express written permission
-// of Qualcomm Technologies, Inc.
-
-// Qualcomm is a trademark of QUALCOMM Incorporated, registered in the United States and other countries. All
-// QUALCOMM Incorporated trademarks are used with permission.
-
-// This software may be subject to U.S. and international export, re-export, or transfer laws.  Diversion contrary to U.S.
-// and international law is strictly prohibited.
-
-// Qualcomm Technologies, Inc.
-// 5775 Morehouse Drive
-// San Diego, CA 92121 U.S.A.
-// Copyright © 2013-2015 Qualcomm Technologies, Inc.
-// All rights reserved.
-// Qualcomm Technologies Confidential and Proprietary
-
-/*
-	$Id: //depot/QDL/QCommon/LogInterface.cpp#1 $
-	$Header: //depot/QDL/QCommon/LogInterface.cpp#1 $
-	$Date: 2016/08/30 $
-	$DateTime: 2016/08/30 12:45:17 $
-	$Change: 902319 $
-	$File: //depot/QDL/QCommon/LogInterface.cpp $
-	$Revision: #1 $
-	%Author: msimpson %
-*/
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause
 
 #include "LogInterface.h"
 
--- SCLDump/SCLDump.pro
+++ SCLDump/SCLDump.pro
@@ -1,3 +1,6 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
 include(../QCommonConsole/Common.pri)
 
 QT += core serialport network
